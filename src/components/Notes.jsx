361f43d6e3dfb29d625a0346886c8e54b34998e2
bgetson <bgetson@twilio.com>
2019-07-22 21:36:58 -0700

    v3.0 - server-side notes

--- src/components/Notes.jsx
+++ src/components/Notes.jsx
@@ -1,20 +1,31 @@
 import React, { Component } from 'react';
 import Card from '@material-ui/core/Card';
 import { NotesStyles } from './Notes.Styles';
+import axios from 'axios';
 
 export default class Notes extends Component {
     constructor() {
         super();
 
         this.state = {
-            notes: localStorage.getItem('agentNotes'),
+            notes: '',
         }
+    }
+
+    componentDidMount() {
+        axios.get('http://localhost:3001/notes')
+            .then((response) => {
+                this.setState({ notes: response.data.notes });
+            })
+            .catch((error) => {
+                this.setState({ notes: "Couldn't load notes..." });
+            });
     }
 
     handleTextareaOnChange = (e) => {
         const notes = e.target.value;
 
-        localStorage.setItem('agentNotes', notes);
+        axios.post('http://localhost:3001/notes', { notes });
         this.setState({ notes });
     }
 
